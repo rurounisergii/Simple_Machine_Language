9466f34951b0579cd2bb90aca0f87d281cce2bc6
Sergio <rurounisergii@hotmail.com>
2015-01-30 13:56:22 +0000

    Layout of test completed, but now need to find a way to send command line arguments to the Machine object i have made to give it the appropriate fileName to pass to Translator constructor.

--- src/smlTests/AddInstructionTest.java
+++ src/smlTests/AddInstructionTest.java
@@ -6,13 +6,17 @@ import sml.*;
 import java.io.*;
 
 public class AddInstructionTest {
-	public String result;
-	String fileName = "src" + File.separator + "addTest.txt";
-	Registers testerRegisters = new Registers();
+	private String result;
+	private String fileName = "src" + File.separator + "addTest.txt";
+	private File file;
+	private Registers testerRegisters = new Registers();
 	
+	/*
+	 * Create a file with instructions to be read by the SML Machine
+	 */
 	@Before
-	public void buildup(){
-		File file = new File(fileName);
+	public void buildUp(){
+		file = new File(fileName);
 		try (PrintWriter out = new PrintWriter(file)){
 			out.println("L1 lin 0 5");
 			out.println("L2 lin 1 7");
@@ -22,14 +26,28 @@ public class AddInstructionTest {
 			} catch (IOException ex) {
 			ex.printStackTrace();
 			} 	
-		System.out.println(file.exists());
+		
 	}
 	
+	/*
+	 * Delete the file once tests are done
+	 */
+	@After
+	public void cleanUp(){
+		file.delete();
+	}
+	
+	/*
+	 * Add expected values into testerRegisters based on the file created in @Before
+	 * Create a new Machine with name of the appropriate file as command line arguments
+	 * Check expected registers are the same as the Machine registers
+	 */
 	@Test
 	public void addTest(){
-		assertEquals(1,1);
+		testerRegisters.setRegister(0, 5);
+		testerRegisters.setRegister(1, 7);
+		testerRegisters.setRegister(2, 12);
+		Machine testerMachine = new Machine();
+		assertEquals(testerRegisters, testerMachine.getRegisters());
 	}
-	
-	
-
 }
