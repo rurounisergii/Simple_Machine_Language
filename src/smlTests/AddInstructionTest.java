8a84b3361da589c20416449cc6d6cae724757f62
Sergio <rurounisergii@hotmail.com>
2015-01-30 14:37:22 +0000

    Finished junit test for AddInstruction. Test passes as expected

--- src/smlTests/AddInstructionTest.java
+++ src/smlTests/AddInstructionTest.java
@@ -6,10 +6,11 @@ import sml.*;
 import java.io.*;
 
 public class AddInstructionTest {
-	private String result;
 	private String fileName = "src" + File.separator + "addTest.txt";
 	private File file;
 	private Registers testerRegisters = new Registers();
+	private Machine testerMachine;
+	
 	
 	/*
 	 * Create a file with instructions to be read by the SML Machine
@@ -18,9 +19,9 @@ public class AddInstructionTest {
 	public void buildUp(){
 		file = new File(fileName);
 		try (PrintWriter out = new PrintWriter(file)){
-			out.println("L1 lin 0 5");
-			out.println("L2 lin 1 7");
-			out.println("L3 add 2 1 2"); 
+			out.println("f0 lin 0 5");
+			out.println("f1 lin 1 7");
+			out.println("f2 add 2 0 1"); 
 			} catch (FileNotFoundException ex) {
 				ex.printStackTrace();;
 			} catch (IOException ex) {
@@ -38,16 +39,19 @@ public class AddInstructionTest {
 	}
 	
 	/*
-	 * Add expected values into testerRegisters based on the file created in @Before
-	 * Create a new Machine with name of the appropriate file as command line arguments
-	 * Check expected registers are the same as the Machine registers
+	 * Add expected final register values to testerRegisters
+	 * Create a Machine and Translator to process the lines written to a file in @Before
+	 * The Machine and Translator should be able to correctly the contained instructions
 	 */
 	@Test
 	public void addTest(){
 		testerRegisters.setRegister(0, 5);
 		testerRegisters.setRegister(1, 7);
 		testerRegisters.setRegister(2, 12);
-		Machine testerMachine = new Machine();
+		testerMachine = new Machine();
+		Translator TranslatorTester = new Translator("addTest.txt");
+		TranslatorTester.readAndTranslate(testerMachine.getLabels(), testerMachine.getProg());
+		testerMachine.execute();
 		assertEquals(testerRegisters, testerMachine.getRegisters());
 	}
 }
