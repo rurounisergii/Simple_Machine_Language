6da7d24a8aa244d94f6b0c9447a37ee1b7e1875e
Sergio <rurounisergii@hotmail.com>
2015-01-30 13:32:28 +0000

    Will test instructions by creating files, then deleting them after test in @Before and @After

--- src/smlTests/AddInstructionTest.java
+++ src/smlTests/AddInstructionTest.java
@@ -3,19 +3,33 @@ package smlTests;
 import org.junit.*;
 import static org.junit.Assert.*;
 import sml.*;
-import java.io.File;
+import java.io.*;
 
 public class AddInstructionTest {
 	public String result;
-	String fileName = "src" + File.separator + "tester.txt";
+	String fileName = "src" + File.separator + "addTest.txt";
 	Registers testerRegisters = new Registers();
 	
-	@BeforeClass
+	@Before
 	public void buildup(){
 		File file = new File(fileName);
-		
-		
+		try (PrintWriter out = new PrintWriter(file)){
+			out.println("L1 lin 0 5");
+			out.println("L2 lin 1 7");
+			out.println("L3 add 2 1 2"); 
+			} catch (FileNotFoundException ex) {
+				ex.printStackTrace();;
+			} catch (IOException ex) {
+			ex.printStackTrace();
+			} 	
+		System.out.println(file.exists());
 	}
+	
+	@Test
+	public void addTest(){
+		assertEquals(1,1);
+	}
+	
 	
 
 }
