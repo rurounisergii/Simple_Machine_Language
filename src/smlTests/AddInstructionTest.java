99424aa43cd23ca032c79b5907452243a1a9c7d4
Sergio <rurounisergii@hotmail.com>
2015-01-30 12:59:07 +0000

    We were told in class that simple tests were not enough. Proper junit tests were required so i have now begun AddInstructionTest

---
+++ src/smlTests/AddInstructionTest.java
@@ -0,0 +1,21 @@
+package smlTests;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+import sml.*;
+import java.io.File;
+
+public class AddInstructionTest {
+	public String result;
+	String fileName = "src" + File.separator + "tester.txt";
+	Registers testerRegisters = new Registers();
+	
+	@BeforeClass
+	public void buildup(){
+		File file = new File(fileName);
+		
+		
+	}
+	
+
+}
