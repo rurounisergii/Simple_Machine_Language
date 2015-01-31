14dcc01c4dde544f75bbf07d6a3e13351683fa6c
Sergio <rurounisergii@hotmail.com>
2015-01-31 19:04:17 +0000

    Almost fully figured out how i will implement this. How need to find a way to cast the object into multiplyInstruction class

--- src/sml/testingReflection.java
+++ src/sml/testingReflection.java
@@ -3,6 +3,7 @@ package sml;
 import java.io.File;
 import java.io.FilenameFilter;
 import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 
 public class testingReflection {
@@ -36,21 +37,42 @@ public class testingReflection {
 		file.delete();	
 	
 		System.out.println(this.getClass());
-		
+		String classname = "sml.MultiplyInstruction";
 		try {
 			Class<?> clazz = Class.forName("sml.MultiplyInstruction");
 			Constructor[] allConstructors = clazz.getDeclaredConstructors(); 
 			Class[] constructParaneters = allConstructors[0].getParameterTypes();
 			try {
 				Constructor c = clazz.getConstructor(new Class[]{String.class, Integer.TYPE, Integer.TYPE, Integer.TYPE});
-				System.out.println("Consturcot: "+ c);
+				System.out.println("Consturcot 1: "+ c);
+				Object[] arguments = new Object[4];
+				arguments[0] = "Hello";
+				arguments[1] = new Integer(1);
+				arguments[2] = new Integer(1);
+				arguments[3] = new Integer(1);
 				Class[] types1 = new Class[4];
 				types1[0] = String.class;
 				types1[1] = Integer.TYPE;
 				types1[2] = Integer.TYPE;
 				types1[3] = Integer.TYPE;
-				Constructor d = clazz.getConstructor(types1);
-				System.out.println("Consturcot: "+ d);
+				Object d;
+				try {
+					d = clazz.getConstructor(types1).newInstance(arguments);
+					clazz = (sml.MultiplyInstruction) d;
+				} catch (InstantiationException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IllegalAccessException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IllegalArgumentException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (InvocationTargetException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				System.out.println("Consturcot 2: "+ d);
 			} catch (NoSuchMethodException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -60,18 +82,7 @@ public class testingReflection {
 			}
 			
 			
-			/*Object[] parameterHolder = new Object[4];
-			parameterHolder[0] = ("String");
-			parameterHolder[1] = (new Integer(4));
-			parameterHolder[2] = (new Integer(4));
-			parameterHolder[3] = (new Integer(4));
-			
-			Class[] classTypes = new Class[parameterHolder.size()];
-			for (int x = 0; x < parameterHolder.length; x++){
-				Class<?> c = Class.forName(parameterHolder[x]);
-				classTypes[x] = c.getClass();
-			}
-			*/
+
 			for (Constructor x : allConstructors){
 				System.out.println(x);
 			}
